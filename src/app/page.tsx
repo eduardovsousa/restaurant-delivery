81cb04de669c7709149b49845dd605cac7c86191
eduardovsousa <eduardo.varela@outlook.com.br>
2025-02-26 22:32:30 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <span>Home</span>
+    <div className="p-5 rounded-xl">
+      <span className="text-red-500 p-5">Home</span>
     </div>
   );
 }
