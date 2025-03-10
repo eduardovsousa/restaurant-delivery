0f9e7b3103ad32f17c63a671956b0d52c884de29
eduardovsousa <eduardo.varela@outlook.com.br>
2025-03-10 20:23:22 -0300

    feat: ensure cart summary is shown on restaurant page

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -3,11 +3,14 @@
 import { Prisma } from "@prisma/client";
 import { ClockIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { formatCurrency } from "@/helpers/format-currency";
 
+import { CartContext } from "../contexts/cart";
+import CartSheet from "./cart-sheet";
 import Products from "./products";
 
 interface RestaurantCategoriesProps {
@@ -25,7 +28,9 @@ type MenuCategoriesWithProducts = Prisma.MenuCategoryGetPayload<{
 }>
 
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
-  const [selectedCategory, setSelectedCategory] = useState<MenuCategoriesWithProducts>(restaurant.menuCategories[0])
+  const [selectedCategory, setSelectedCategory] = useState<MenuCategoriesWithProducts>(restaurant.menuCategories[0]);
+
+  const { products, total, toggleCart, totalQuantity } = useContext(CartContext);
 
   const handleCategoryClick = (category: MenuCategoriesWithProducts) => {
     setSelectedCategory(category)
@@ -65,6 +70,22 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
 
       <h3 className="px-5 font-semibold pt-2">{selectedCategory.name}</h3>
       <Products products={selectedCategory.products} />
+
+      {products.length > 0 && (
+        <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
+          <div>
+            <p className="text-xs text-muted-foreground">Total do pedido</p>
+            <p className="text-xs font-semibold">
+              {formatCurrency(total)}
+              <span className="text-muted-foreground font-normal"> / {totalQuantity} {totalQuantity === 1 ? "item" : "itens"}</span>
+            </p>
+          </div>
+
+          <Button onClick={toggleCart}>Ver sacola</Button>
+          <CartSheet />
+
+        </div>
+      )}
     </div>
   );
 }
